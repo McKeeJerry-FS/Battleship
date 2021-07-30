b4b4302a51f94286efb0db53055bfa8013d37902
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-07-30 08:49:49 -0400

    created a view method, including three display functions

--- js/battleship.js
+++ js/battleship.js
@@ -10,3 +10,27 @@ var isSunk = false;
 
 
 
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+}
+
+view.displayHit("00");
+view.displayHit("13");
+view.displayHit("36");
+view.displayMiss("34");
+view.displayMiss("16");
+
+view.displayMessage("Testing...Testing... 1, 2, 3");
