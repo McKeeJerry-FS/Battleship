5035796e8a740bf408cae4bbf2ce4fe6d135a682
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2021-07-29 22:05:33 -0400

    resetting JS

--- js/battleship.js
+++ js/battleship.js
@@ -8,23 +8,5 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false) {
-    guess = prompt("Ready, Aim, Fire! (Enter a number between 0-6): ");
-    if (guess < 0 || guess > 6) {
-        alert("Please enter a valid cell number between 0 - 6")
-    } else {
-        guesses = guesses + 1;
-    }
 
-    if (guess == location1 || guess == location2 || guess == location3) {
-        hits = hits + 1;
-    }
 
-    if (hits == 3 ) {
-        isSunk = true;
-        alert("You Sank My Battleship!");
-    }
-}
-
-var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shotting accuracy was " + (3/guesses) + "%.";
-alert(stats);
